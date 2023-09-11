dd9528e715870f8028f12f476802a39fce8fc9c8
Hakan <ahmethakanozturk34@gmail.com>
2023-09-11 15:51:58 +0300

    First Lecture's Materials
    -Adding new projects for the first lecture's materials

---
+++ Lecture_1/InsecureBank/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lecture_1.InsecureBank;
+class Program
+{
+    static List<string> accountOwners = new List<string>();
+    static List<double> accountBalances = new List<double>();
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Welcome to the Insecure Bank!");
+            Console.WriteLine("1. Create an Account");
+            Console.WriteLine("2. Deposit Money");
+            Console.WriteLine("3. Withdraw Money");
+            Console.WriteLine("4. Check Balance");
+            Console.WriteLine("5. Exit");
+            Console.Write("Enter your choice: ");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    CreateAccount();
+                    break;
+                case 2:
+                    DepositMoney();
+                    break;
+                case 3:
+                    WithdrawMoney();
+                    break;
+                case 4:
+                    CheckBalance();
+                    break;
+                case 5:
+                    Console.WriteLine("Thank you for using the Insecure Bank!");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+
+    static void CreateAccount()
+    {
+        Console.Write("Enter your name: ");
+        string ownerName = Console.ReadLine();
+
+        accountOwners.Add(ownerName);
+        accountBalances.Add(0.0);
+
+        Console.WriteLine("Account created successfully!");
+    }
+
+    static void DepositMoney()
+    {
+        Console.Write("Enter your name: ");
+        string ownerName = Console.ReadLine();
+        int index = accountOwners.IndexOf(ownerName);
+
+        Console.Write("Enter the amount to deposit: ");
+        double amount = double.Parse(Console.ReadLine());
+        accountBalances[index] += amount;
+        Console.WriteLine($"Deposited ${amount}. New balance: ${accountBalances[index]}");
+
+    }
+
+    static void WithdrawMoney()
+    {
+        Console.Write("Enter your name: ");
+        string ownerName = Console.ReadLine();
+        int index = accountOwners.IndexOf(ownerName);
+
+        Console.Write("Enter the amount to withdraw: ");
+        double amount = double.Parse(Console.ReadLine());
+
+        if (accountBalances[index] >= amount)
+        {
+            accountBalances[index] -= amount;
+            Console.WriteLine($"Withdrawn ${amount}. New balance: ${accountBalances[index]}");
+        }
+        else
+        {
+            Console.WriteLine("Insufficient funds.");
+        }
+    }
+
+    static void CheckBalance()
+    {
+        Console.Write("Enter your name: ");
+        string ownerName = Console.ReadLine();
+        int index = accountOwners.IndexOf(ownerName);
+
+        Console.WriteLine($"Your balance is ${accountBalances[index]}");
+    }
+}
---
+++ Lecture_1/ZamazonDelivery/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static List<string> customers = new List<string>();
+    static List<string> orders = new List<string>();
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Zamazon Customer and Order Management System");
+            Console.WriteLine("1. Add Customer");
+            Console.WriteLine("2. Place Order");
+            Console.WriteLine("3. View Orders");
+            Console.WriteLine("4. Exit");
+            Console.Write("Enter your choice: ");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    AddCustomer();
+                    break;
+                case 2:
+                    PlaceOrder();
+                    break;
+                case 3:
+                    ViewOrders();
+                    break;
+                case 4:
+                    Console.WriteLine("Goodbye!");
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+
+    static void AddCustomer()
+    {
+        Console.Write("Enter customer name: ");
+        string customerName = Console.ReadLine();
+        customers.Add(customerName);
+        Console.WriteLine($"Customer '{customerName}' added successfully!");
+    }
+
+    static void PlaceOrder()
+    {
+        Console.Write("Enter customer name: ");
+        string customerName = Console.ReadLine();
+
+        Console.Write("Enter order details: ");
+        string orderDetails = Console.ReadLine();
+        orders.Add($"{customerName}: {orderDetails}");
+        Console.WriteLine("Order placed successfully!");
+
+    }
+
+    static void ViewOrders()
+    {
+        Console.WriteLine("Orders:");
+        foreach (var order in orders)
+        {
+            Console.WriteLine(order);
+        }
+    }
+}
