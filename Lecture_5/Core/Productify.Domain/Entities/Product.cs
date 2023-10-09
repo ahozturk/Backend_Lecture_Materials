0865cf49f9df5baf74ea95d71a9393881f282f75
Hakan <ahmethakanozturk34@gmail.com>
2023-10-09 21:09:25 +0300

    Lecture 5 - First Part

---
+++ Lecture_5/Core/Productify.Domain/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productify.Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedOn { get; set; }
+      
+        public Product(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            CreatedOn = DateTime.Now;
+        }
+    }
+}
---
+++ Lecture_5/Presentation/Productify.MVC/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Productify.Domain.Entities;
+
+namespace Productify.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        List<Product> _products = new()
+        {
+            new Product("Laptop Computer"),
+            new Product("Smartphone"),
+            new Product("Wireless Headphones"),
+            new Product("Digital Camera"),
+            new Product("Tablet")
+        };
+
+        public IActionResult GetAll()
+        {
+            return View(_products);
+        }
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+    }
+}
