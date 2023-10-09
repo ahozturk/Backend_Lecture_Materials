1c9d4adc21a4aa12dd53cc2d30a8737a5b062565
Hakan <ahmethakanozturk34@gmail.com>
2023-10-09 22:48:47 +0300

    End of Lesson

---
+++ Lecture_5/Infrastructure/Persistence/Contexts/ProductifyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Productify.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productify.Persistence.Contexts
+{
+    public class ProductifyDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("ProductifyDb");
+        }
+    }
+}
--- Lecture_5/Presentation/Productify.MVC/Controllers/ProductController.cs
+++ Lecture_5/Presentation/Productify.MVC/Controllers/ProductController.cs
@@ -1,26 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Productify.Domain.Entities;
+using Productify.Persistence.Contexts;
 
 namespace Productify.MVC.Controllers
 {
     public class ProductController : Controller
     {
-        List<Product> _products = new()
+        //List<Product> _products = new()
+        //{
+        //    new Product("Laptop Computer"),
+        //    new Product("Smartphone"),
+        //    new Product("Wireless Headphones"),
+        //    new Product("Digital Camera"),
+        //    new Product("Tablet")
+        //};
+
+        ProductifyDbContext _context;
+
+        public ProductController()
         {
-            new Product("Laptop Computer"),
-            new Product("Smartphone"),
-            new Product("Wireless Headphones"),
-            new Product("Digital Camera"),
-            new Product("Tablet")
-        };
+            _context = new();
+        }
 
         public IActionResult GetAll()
         {
-            return View(_products);
+            return View(_context.Products.ToList());
         }
 
+        [HttpGet]
         public IActionResult AddProduct()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(string productName)
+        {
+            _context.Products.Add(new(productName));
+
+            _context.SaveChanges();
+
             return View();
         }
     }
