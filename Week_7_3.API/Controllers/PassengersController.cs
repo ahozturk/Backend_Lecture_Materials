074d49024e3b058163e4179111cee102a5bf4250
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-10-26 21:50:01 +0300

    passenger add and list controller metods added

---
+++ Week_7_3.API/Controllers/PassengersController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Week_7_3.API.Models;
+using Week_7_3.Domain.Entities;
+using Week_7_3.Persistence.Contexts;
+
+namespace Week_7_3.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PassengersController : ControllerBase
+    {
+        readonly Week_7_3DbContext _context;
+
+        public PassengersController(Week_7_3DbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet("[action]")]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Passengers.ToList());
+        }
+
+        [HttpPost("[action]")]
+
+        public IActionResult CreatePassenger([FromBody] CreatePassengerRequest createPassengerRequest)
+        {
+            Passenger passenger = new()
+            {
+                Name = createPassengerRequest.Name,
+                Surname = createPassengerRequest.Surname,
+                PhoneNumber = createPassengerRequest.PhoneNumber,
+                City = createPassengerRequest.City,
+                Account = createPassengerRequest.Account,
+    };
+
+            _context.Passengers.Add(passenger);
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+
+    }
+}
---
+++ Week_7_3.API/Models/CreatePassengerRequest.cs
@@ -0,0 +1,14 @@
+﻿using Week_7_3.Domain.Entities;
+
+namespace Week_7_3.API.Models
+{
+    public class CreatePassengerRequest
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string City { get; set; }
+        public BankAccount? Account { get; set; }
+    }  
+}
+
