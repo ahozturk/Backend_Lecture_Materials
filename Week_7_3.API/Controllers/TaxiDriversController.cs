f403b2f9a79eac2afd46022206765aa40dafe0ab
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-10-26 21:59:43 +0300

    taxi driver controller added

---
+++ Week_7_3.API/Controllers/TaxiDriversController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Week_7_3.API.Models;
+using Week_7_3.Domain.Entities;
+using Week_7_3.Persistence.Contexts;
+
+namespace Week_7_3.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaxiDriversController : ControllerBase
+    {
+        readonly Week_7_3DbContext _context;
+
+        public TaxiDriversController()
+        {
+            _context = new Week_7_3DbContext();
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.TaxiDrivers.ToList());
+        }
+        [HttpPost("[action]")]
+
+        public IActionResult CreateTaxiDriver([FromBody] CreateTaxiDriverRequest taxiDriver)
+        {
+            TaxiDriver taxiDriver1 = new()
+            {
+                Name = taxiDriver.Name,
+                Surname = taxiDriver.Surname,
+                PhoneNumber = taxiDriver.PhoneNumber,
+                Account = taxiDriver.Account,
+                LicencePlate = taxiDriver.LicencePlate,
+            };
+
+            _context.TaxiDrivers.Add(taxiDriver1);
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ Week_7_3.API/Models/CreateTaxiDriverRequest.cs
@@ -0,0 +1,13 @@
+﻿using Week_7_3.Domain.Entities;
+
+namespace Week_7_3.API.Models
+{
+    public class CreateTaxiDriverRequest
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public BankAccount? Account { get; set; }
+        public string LicencePlate { get; set; }
+    }
+}
